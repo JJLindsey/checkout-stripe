8fd84996e45b81a10fd53a032261734cc1908507
j_lindsey <jlindsey010@outlook.com>
2024-12-10 15:45:07 -0800

    add button functionality to cards

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ import Success from './pages/Success'
 import Cancel from './pages/Cancel'
 import Store from './pages/Store'
 import { Box } from '@mui/material'
+import CartProvider from './CartContext';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navigation />
-      <Box sx={{ml: 4, pt: 10}}>
-      <Routes>
-        <Route index element={<Store/>} />
-        <Route path='success' element={<Success />} />
-        <Route path='cancel' element={<Cancel />} />
-      </Routes>
-      </Box>
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter>
+        <Navigation />
+        <Box sx={{ml: 4, pt: 10}}>
+        <Routes>
+          <Route index element={<Store/>} />
+          <Route path='success' element={<Success />} />
+          <Route path='cancel' element={<Cancel />} />
+        </Routes>
+        </Box>
+      </BrowserRouter>
+    </CartProvider>
   );
 }
 
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,8 +1,15 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material'
-import React from 'react'
+import React, {useContext} from 'react'
+import { CartContext } from '../CartContext'
+import { Button, Card, CardActions, CardContent, CardMedia, IconButton, Typography } from '@mui/material'
+import AddCircleIcon from '@mui/icons-material/AddCircle'
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
 
 export default function ProductCard(props) {
     const product = props.product
+    const cart = useContext(CartContext)
+    const productQuantity = cart.getProductQuantity(product.id)
+    console.log(cart.items)
+
   return (
     <Card>
         <CardMedia title='card image' />
@@ -13,7 +20,17 @@ export default function ProductCard(props) {
             <Typography>{product.price}</Typography>
         </CardContent>
         <CardActions>
-            <Button>Add to Cart</Button>
+            {productQuantity > 0 ?
+            <>
+                <Typography>In Cart: {productQuantity}</Typography>
+                <IconButton variant='contained' color='primary' size='small' onClick={() => cart.addOneToCart(product.id)}><AddCircleIcon /></IconButton>
+                <IconButton variant='contained' color='primary' size='small' onClick={() => cart.removeOneFromCart(product.id)}><RemoveCircleIcon /></IconButton>
+                <Button color='error' variant='contained' size='small' onClick={() => cart.deleteFromCart(product.id)} className="my-2">Remove from cart</Button>
+            </>
+            :
+            <Button onClick={() => cart.addOneToCart(product.id)}>Add to Cart</Button>
+            }
+            
         </CardActions>
     </Card>
   )
